78f8ebddc2988f93cc9add59dd234e1ab46ff357
Pedro Galhardo <pedromgalhardo@tecnico.ulisboa.pt>
2019-10-28 23:33:23 +0000

    Feature/disappearing-dots (#31)
    * Proj3: Initial commit
    
    * Remove some old stuff
    
    * Rename variable
    
    * Disappearing dots: Initial commit
    
    * Fix dot geometry
    
    * Disappearing dots: add paint and frame

--- Proj3/js/main.js
+++ Proj3/js/main.js
@@ -2,11 +2,96 @@ var scene, renderer, currCamera, viewSize = 4/5;
 var sceneCamera, opArtCamera, camera;
 var controls;
 
-var dotsColor = 0xffffff, backgroundColor = 0x000000;
-var wireframeOn = true;
+var materials = [];
+var wireframeOn = false;
+
+//Scene
+var backgroundColor = 0x000000, floorColor = 0x727982, objectDepth = 1;
+
+//Wall
+var wallColor = 0xbdbcba, wallWidth = 130, wallHeight = 100;
+
+//Frame
+var dotRadius = 1;
+var dotsColor = 0xffffff, paintColor = 0x858585, squareColor = 0x000000, frameColor = 0x653815;
 
 var width = window.innerWidth, height = window.innerHeight;
 var oldWidth = width, oldHeight = height;
+
+class Dot extends THREE.Object3D {
+    constructor() {
+        super();
+        var geometry = new THREE.CylinderGeometry(dotRadius, dotRadius, objectDepth, 64, 1);
+        var material = new THREE.MeshBasicMaterial( {color:dotsColor, wireframe: wireframeOn} );
+        materials.push(material);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.rotateX(-Math.PI / 2);
+        this.add(this.mesh);
+    }
+
+    setPosition(x, y, z) {
+        this.position.set(x, y, z);
+    }
+}
+
+class Wall extends THREE.Object3D {
+    constructor(x, y, z, width, height) {
+        super();
+        this.position.set(x, y, z);
+
+        var material = new THREE.MeshBasicMaterial( {color: wallColor, wireframe: wireframeOn} );
+        materials.push(material);
+        var geometry = new THREE.BoxGeometry(width, height, objectDepth);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.add(this.mesh);
+    }
+}
+
+class Paint extends THREE.Object3D {
+    constructor(x, y, z, width, height) {
+        super();
+        this.position.set(x, y, z);
+
+        var material = new THREE.MeshBasicMaterial( {color: paintColor, wireframe: wireframeOn} );
+        materials.push(material);
+        var geometry = new THREE.BoxGeometry(width, height, objectDepth);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.add(this.mesh);
+    }
+}
+
+class Frame extends THREE.Object3D {
+    constructor(x, y, z, width, height) {
+        super();
+        this.position.set(x, y, z);
+
+        var material = new THREE.MeshBasicMaterial( {color: frameColor, wireframe: wireframeOn} );
+        
+        // solve Z-fighting problem
+        material.polygonOffset = true;
+        material.depthTest = true;
+        material.polygonOffsetFactor = 1;
+        material.polygonOffsetUnits = 0.1;
+       
+        materials.push(material);
+        var geometry = new THREE.BoxGeometry(width, height, objectDepth * 3);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.add(this.mesh);
+    }
+}
+
+class Square extends THREE.Object3D {
+    constructor(x, y, z, width, height) {
+        super();
+        this.position.set(x, y, z);
+
+        var material = new THREE.MeshBasicMaterial( {color: squareColor, wireframe: wireframeOn} );
+        materials.push(material);
+        var geometry = new THREE.BoxGeometry(width, height, objectDepth);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.add(this.mesh);
+    }
+} 
 
 class OrtCamera extends THREE.OrthographicCamera {
     constructor(x, y, z, lookx, looky, looz) {
@@ -45,6 +130,61 @@ class PerCamera extends THREE.PerspectiveCamera {
     }
 }
 
+function createWall() {
+	var wall = new Wall(wallWidth / 2, wallHeight / 2, objectDepth / 2, wallWidth, wallHeight)
+	scene.add(wall)
+}
+
+function createPaint() {
+    let width = 110, height = 80
+    var paint = new Paint(width / 2 + 10, height / 2 + 10, objectDepth, width, height)
+    scene.add(paint)
+
+    createFrame(9, (wallHeight - 20) / 2 + 10, objectDepth * 2, 3, wallHeight - 20)
+    createFrame(9 + width + 2, (wallHeight - 20) / 2 + 10, objectDepth * 2, 3, wallHeight - 20)
+    createFrame(9 + (wallWidth - 18) / 2, height + 10, objectDepth * 2, wallWidth - 15, 3)
+    createFrame(9 + (wallWidth - 18) / 2, 10, objectDepth * 2, wallWidth - 15, 3)
+}
+
+function createFrame(x, y, z, width, height) {
+    var frame = new Frame(x, y, z, width, height)
+    scene.add(frame)
+}
+
+function createDots(width, height) {
+	var x = y = 20
+	for (var i = 0; i < width; i++, x += 10) {
+		y = 20
+		for (var j = 0; j < height; j++, y += 10) {
+			var dot = new Dot()
+			dot.setPosition(x, y, objectDepth * 2);
+			scene.add(dot)
+		}
+	}
+}
+
+function createSquares(width, height) {
+	var x = Math.cos(Math.PI / 4) + 10
+	for (var i = 0; i <= width; i++, x += 10) {
+		var y = Math.cos(Math.PI / 4) + 10
+		for (var j = 0; j <= height; j++, y += 10) {
+            let squareWidth = 10 - 2 * Math.cos(Math.PI / 4)
+            let squareHeight = 10 - 2 * Math.cos(Math.PI / 4)
+			var square = new Square(x + squareWidth / 2, y + squareHeight / 2, objectDepth * 2, squareWidth, squareHeight);
+			scene.add(square)
+		}
+	}
+}
+
+function createFloor() {
+    var geometry = new THREE.PlaneGeometry(260, 100);
+    var material = new THREE.MeshBasicMaterial( {color: floorColor, side: THREE.DoubleSide} ); 
+    var plane = new THREE.Mesh( geometry, material );
+    plane.rotateX( - Math.PI / 2);
+    plane.position.set(0, 0, 50);
+    scene.add(plane);
+}
+
 function createPerspectiveCamera() {
     'use strict';
 
@@ -62,6 +202,12 @@ function createScene() {
 
     scene = new THREE.Scene();
     scene.add(new THREE.AxesHelper(100));
+
+    createFloor()
+    createWall()
+    createPaint()
+    createDots(10, 7)
+    createSquares(10, 7)
 }
 
 function onResize() {
