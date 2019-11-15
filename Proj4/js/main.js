3f186c65d978bd529aeaa7b80a3aaee121e5a641
Pedro Galhardo <pedromgalhardo@tecnico.ulisboa.pt>
2019-11-15 23:43:49 +0000

    Proj4/dice (#36)
    * Add dice and pause overlay
    
    * Add update function using delta
    
    * Refactor: keydown handling; misc cleanup
    
    * Fix dice rotation; general review and cleanup
    
    * Fix date
    
    * Add TODOs
    
    * Misc cleanup
    
    * Remove deprecated camera

--- Proj4/js/main.js
+++ Proj4/js/main.js
@@ -1,31 +1,54 @@
-var scene, renderer, viewSize = 4/5;
-var controls, objects = [];
+/*
+// main.js - Proj4
+// Computação Gráfica, Grupo 22 2019-20
+*/
 
-var wireframeOn = false, isPaused = false;
-
-var backgroundColor = 0x000000;
-
-var width = window.innerWidth, height = window.innerHeight;
-var oldWidth = width, oldHeight = height;
+/*
+// TODOs
+// Fix pause overlay resize
+// Edit dice texture
+// Reset dice
+// Review delta
+*/
 
 /*==============================================================================
 	Modulation
 ==============================================================================*/
+
+var objects = [];
+
 class THREEJSObject extends THREE.Object3D {
     constructor() {
         super();
     }
 
     createBasicMaterial(texture, color = 0xffffff) {
-        this.basicMaterial = new THREE.MeshBasicMaterial( {color: color, side: THREE.DoubleSide, map: texture, wireframe: wireframeOn} );
+        this.basicMaterial = new THREE.MeshBasicMaterial( {
+        	color: color,
+        	side: THREE.DoubleSide,
+        	map: texture,
+        	wireframe: false
+        } );
     }
 
     createPhongMaterial(texture, bumpMap, color = 0xffffff) {
-        this.phongMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff, side: THREE.DoubleSide, map: texture, bumpMap: bumpMap, wireframe: wireframeOn} );
+        this.phongMaterial = new THREE.MeshPhongMaterial( {
+        	color: 0xffffff,
+        	side: THREE.DoubleSide,
+        	map: texture,
+        	bumpMap: bumpMap,
+        	wireframe: false
+        } );
     }
 
     createLambertMaterial(color = 0xffffff, texture, bumpMap) {
-        this.lambertMaterial = new THREE.MeshLambertMaterial( {color: color, side: THREE.DoubleSide, map: texture, bumpMap: bumpMap, wireframe: wireframeOn} );
+        this.lambertMaterial = new THREE.MeshLambertMaterial( {
+        	color: color,
+        	side: THREE.DoubleSide,
+        	map: texture,
+        	bumpMap: bumpMap,
+        	wireframe: false
+        } );
     }
 
     setBasicMaterial() {
@@ -52,34 +75,107 @@ class THREEJSObject extends THREE.Object3D {
         return this.lambertMaterial;
     }
 
-    changeWireframe() {
-        this.basicMaterial.wireframe = wireframeOn;
-        this.phongMaterial.wireframe = wireframeOn;
-        //this.lambertMaterial.wireframe = wireframeOn;
+    toggleWireframe() {
+        this.basicMaterial.wireframe = !this.basicMaterial.wireframe;
+        this.phongMaterial.wireframe = !this.phongMaterial.wireframe;
+        //this.lambertMaterial.wireframe = !this.lambertMaterial.wireframe;
     }
 }
 
-class Board extends THREEJSObject{
+class Board extends THREEJSObject {
     constructor() {
         super();
-        this.boardtexture = new textureLoader.load("resources/chess_texture.jpg");
-        this.boardBumpMap = new textureLoader.load("resources/wood_bump_map.jpg");
+        this.texture = new textureLoader.load("resources/chess_texture.jpg");
+        this.bumpMap = new textureLoader.load("resources/wood_bump_map.jpg");
         this.geometry = new THREE.PlaneGeometry(100 , 100, 10, 10);
-        this.createBasicMaterial(this.boardtexture);
-        this.createPhongMaterial(this.boardtexture, this.boardBumpMap);
+        this.createBasicMaterial(this.texture);
+        this.createPhongMaterial(this.texture, this.bumpMap);
         this.mesh = new THREE.Mesh(this.geometry, this.getPhongMaterial());
         this.mesh.rotateX( - Math.PI / 2);
         this.add(this.mesh);
-        this.position.set(50, 0, 50);
+        this.position.set(0, 0, 0);
         objects.push(this);
     }
+}
+
+class Dice extends THREEJSObject {
+    constructor() {
+        super();
+        this.geometry = new THREE.CubeGeometry(10, 10, 10);
+        this.texture = new textureLoader.load("resources/dice-bumpmap.jpg");
+        this.bumpMap = this.texture;
+        this.rotationAxis = new THREE.Vector3(0, 1, 0)
+
+        this.faces = []
+		this.mapTextures()
+
+        this.createBasicMaterial(this.texture);
+        this.createPhongMaterial(this.texture, this.bumpMap);
+        this.mesh = new THREE.Mesh(this.geometry, this.getPhongMaterial());
+
+        // Note: expensive operations turned into const
+        // Angle (radian) for second rotation given by:
+        // THREE.Math.degToRad(Math.atan(1 / Math.sqrt(2)) * 180 / Math.PI)
+        const rotZ = 0.6154797086703874;    
+        this.mesh.rotateX(Math.PI / 4);
+        this.rotateZ(rotZ)
+        this.add(this.mesh);
+        
+        // Distance d between dice cornes given by:
+        // 10 * Math.sqrt(3)
+        const height = 17.32050807568877;
+        this.position.set(0, height / 2, 0);
+        objects.push(this);
+    }
+
+    mapTextures() {
+    	// Iterate over the texture to separate the dice's faces.
+		for (let x = 0.0; x < 1; x += 0.5) {
+			for (let bottom = 2/3; bottom >= 0; bottom -= 1/3) {
+				this.faces.push([
+                    new THREE.Vector2(x, bottom),
+                    new THREE.Vector2(x, bottom + 1/3),
+                    new THREE.Vector2(x + 0.5, bottom + 1/3),
+                    new THREE.Vector2(x + 0.5, bottom)
+                ])
+			}
+		}
+
+		// Insert in order.
+		this.geometry.faceVertexUvs[0] = []; 
+		for (let i in [3, 2, 4, 1, 5, 0]) {
+			const face = this.faces[i];
+            this.geometry.faceVertexUvs[0].push([face[0], face[1], face[3]]);
+            this.geometry.faceVertexUvs[0].push([face[1], face[2], face[3]]);
+		}
+    }
+
+    rotate(delta) {
+        if (delta > 0) {
+            this.rotateOnWorldAxis(this.rotationAxis, delta)
+        }
+	}
 }
 
 /*==============================================================================
 	Cameras
 ==============================================================================*/
 
-var camera, currCamera;
+var camera, pauseCamera;
+
+function createCameras() {
+    createOrthographicCamera();
+    createPerspectiveCamera();
+}
+
+function createOrthographicCamera() {
+	pauseCamera = new THREE.OrthographicCamera(
+		-width / 2, width / 2,
+		height / 2, -height / 2,
+		1, 10
+	);
+    pauseCamera.position.z = 10;
+}
 
 function createPerspectiveCamera() {
     'use strict';
@@ -98,7 +194,6 @@ function createPerspectiveCamera() {
 ==============================================================================*/
 
 var directionalLight, pointLight;
-var toggleDirectionalLights = false, togglePointLights = false;
 var switchingDirectionalLights = false, switchingPointLights = false;
 var directionalLightColor = 0xffffff, pointLightColor = 0xffffff;
 
@@ -111,22 +206,23 @@ function createDirectionalLight() {
 }
 
 function createPointLight() {
-	pointLight = new THREE.PointLight( pointLightColor, 0, 100 );
-    pointLight.position.set( 50, 30, 50 );
+	pointLight = new THREE.PointLight( pointLightColor, 0, 100);
+    pointLight.position.set(0, 30, 0);
     scene.add(pointLight);
 }
 
 /*==============================================================================
 	Scene Creation
 ==============================================================================*/
-var board, toggleMaterials = false, isBasicMaterial = 0, toggleWire = false; 
+
+var board, dice, overlay;
 var textureLoader = new THREE.TextureLoader();
 
 function createScene() {
     'use strict';
 
     scene = new THREE.Scene();
-    scene.add(new THREE.AxesHelper(100));
+    //scene.add(new THREE.AxesHelper(100));
 
     createBoard();
     createBall();
@@ -135,17 +231,37 @@ function createScene() {
     createPointLight();
 }
 
+function createPauseHUD() {
+    'use strict';
+
+    pauseHUD = new THREE.Scene();
+	textureLoader.load("resources/paused.png", createPauseOverlay);
+}
+
+function createPauseOverlay(texture) {
+	const material = new THREE.SpriteMaterial( { map: texture } );
+
+	overlay = new THREE.Sprite(material);
+	overlay.position.set(0, 0, 1);
+	overlay.scale.set(
+		material.map.image.width,
+		material.map.image.height,
+		1
+	);
+
+	pauseHUD.add(overlay);
+}
+
 // uses texture
 function createBall() {
 	//TODO
 }
 
-// uses texture + bump map
 function createDice() {
-	//TODO
+	dice = new Dice();
+	scene.add(dice);
 }
 
-// uses texture + bump map
 function createBoard() {
     board = new Board();
     scene.add(board);
@@ -155,11 +271,18 @@ function createBoard() {
 	Event Listeners
 ==============================================================================*/
 
+var togglePause = false, shouldReset = false, toggleWire = false;
+var toggleMaterials = false, toggleBallMovement = false;
+var togglePointLights = false, toggleDirectionalLights = false;
+var width = window.innerWidth, height = window.innerHeight, viewSize = 4/5;
+
 function onResize() {
     'use strict';
     
-    var angle = oldWidth / oldHeight;
-    var windowVector = new THREE.Vector3(0,0,0);
+    const oldWidth = width, oldHeight = height;
+
+    const angle = width / height;
+    const windowVector = new THREE.Vector3(0,0,0);
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.getSize(windowVector);
@@ -185,8 +308,12 @@ function onResize() {
         camera.updateProjectionMatrix();
     }
 
-    oldWidth = width;
-    oldHeight = height;
+    // Updates pause overlay camera
+	pauseCamera.left = - width / 2;
+	pauseCamera.right = width / 2;
+	pauseCamera.top = height / 2;
+	pauseCamera.bottom = - height / 2;
+	pauseCamera.updateProjectionMatrix();
 }
 
 function onKeyDown(e) {
@@ -194,7 +321,7 @@ function onKeyDown(e) {
 
     switch(e.keyCode) {
         case 66: //B - Ball's movement 
-        	toggleBallMovement();
+        	toggleBallMovement = true;
             break;
         case 68: //D - Directional light
             toggleDirectionalLights = true;
@@ -206,10 +333,10 @@ function onKeyDown(e) {
             togglePointLights = true;
             break;
         case 82: //R - Reset
-        	reset()
+        	shouldReset = true;
             break;
         case 83: //S - Start/Stop
-        	toggleAnimation();
+        	togglePause = true;
             break;
         case 87: //W - Wireframe
             toggleWire = true;
@@ -220,82 +347,33 @@ function onKeyDown(e) {
 }
 
 /*==============================================================================
-	Core functions
+	Update scenes
 ==============================================================================*/
 
-function reset() {
-	
-	if (!isPaused) { return }
-	//TODO
+var timePrev;
+var isPaused = false, isBasicMaterial = false;
+
+function update(delta) {
+    updateStatus();
+
+    if (!isPaused) { updateWorld(delta) }
 }
 
-function toggleAnimation() {
-	isPaused = !isPaused
-}
+function updateStatus() {
+    if (togglePause) {
+        togglePause = false;
+        isPaused = !isPaused;
+    }
 
-function toggleWireframe() {
-    wireframeOn = !wireframeOn;
-    for (var i = 0; i < objects.length; i++) {
-        objects[i].changeWireframe();
+    if (shouldReset) {
+        shouldReset = false;
+        reset();
     }
 }
 
-function toggleLightning() {
-	for (var i = 0; i < objects.length; i++) {
-        if (!isBasicMaterial) objects[i].setBasicMaterial();
-        else objects[i].setPhongMaterial();
-    }
+function updateWorld(delta) {
+    dice.rotate(delta);
 
-    isBasicMaterial = isBasicMaterial == 0 ? 1 : 0;
-}
-
-function toggleDirectionalLight() {
-    if (directionalLight.intensity >= 1 && !switchingDirectionalLights) {
-        switchingDirectionalLights = true;
-        var lightTimeout = setInterval(function() {
-            directionalLight.intensity -= 0.1;
-            if (directionalLight.intensity <= 0) {
-                clearInterval(lightTimeout);
-                switchingDirectionalLights = false;
-            }
-        }, 50);
-    }
-    else if (!switchingDirectionalLights) {
-        switchingDirectionalLights = true;
-        var lightTimeout = setInterval(function() {
-            directionalLight.intensity += 0.1;
-            if (directionalLight.intensity >= 1) {
-                clearInterval(lightTimeout);
-                switchingDirectionalLights = false;
-            }
-        }, 50);
-    }
-}
-
-function togglePointLight() {
-	if (pointLight.intensity >= 1 && !switchingPointLights) {
-        switchingPointLights = true;
-        var lightTimeout = setInterval(function() {
-            pointLight.intensity -= 0.1;
-            if (pointLight.intensity <= 0) {
-                clearInterval(lightTimeout);
-                switchingPointLights = false;
-            }
-        }, 50);
-    }
-    else if (!switchingPointLights) {
-        switchingPointLights = true;
-        var lightTimeout = setInterval(function() {
-            pointLight.intensity += 0.1;
-            if (pointLight.intensity >= 1) {
-                clearInterval(lightTimeout);
-                switchingPointLights = false;
-            }
-        }, 50);
-    }
-}
-
-function checkChanges() {
     if (toggleDirectionalLights) {
         toggleDirectionalLights = false;
         toggleDirectionalLight();
@@ -317,21 +395,113 @@ function checkChanges() {
     }
 }
 
+function reset() {
+	//TODO
+}
+
+function toggleWireframe() {
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].toggleWireframe();
+    }
+}
+
+function toggleLightning() {
+	for (let i = 0; i < objects.length; i++) {
+        if (!isBasicMaterial)
+            objects[i].setBasicMaterial();
+        else
+            objects[i].setPhongMaterial();
+    }
+
+    isBasicMaterial = !isBasicMaterial;
+}
+
+function toggleDirectionalLight() {
+    let lightTimeout
+    
+    if (directionalLight.intensity >= 1 && !switchingDirectionalLights) {
+        switchingDirectionalLights = true;
+        lightTimeout = setInterval(function() {
+            directionalLight.intensity -= 0.1;
+            if (directionalLight.intensity <= 0) {
+                clearInterval(lightTimeout);
+                switchingDirectionalLights = false;
+            }
+        }, 50);
+    }
+    
+    else if (!switchingDirectionalLights) {
+        switchingDirectionalLights = true;
+        lightTimeout = setInterval(function() {
+            directionalLight.intensity += 0.1;
+            if (directionalLight.intensity >= 1) {
+                clearInterval(lightTimeout);
+                switchingDirectionalLights = false;
+            }
+        }, 50);
+    }
+}
+
+function togglePointLight() {
+    let lightTimeout
+
+	if (pointLight.intensity >= 1 && !switchingPointLights) {
+        switchingPointLights = true;
+        lightTimeout = setInterval(function() {
+            pointLight.intensity -= 0.1;
+            if (pointLight.intensity <= 0) {
+                clearInterval(lightTimeout);
+                switchingPointLights = false;
+            }
+        }, 50);
+    }
+
+    else if (!switchingPointLights) {
+        switchingPointLights = true;
+        lightTimeout = setInterval(function() {
+            pointLight.intensity += 0.1;
+            if (pointLight.intensity >= 1) {
+                clearInterval(lightTimeout);
+                switchingPointLights = false;
+            }
+        }, 50);
+    }
+}
+
+/*==============================================================================
+    Core functions
+==============================================================================*/
+
+var scene, pauseHUD, renderer, controls;
+var backgroundColor = 0x000000;
+
 function animate(time) {
     'use strict';
 
+    const delta = (time - timePrev) / 200;
+
     controls.update();
-    checkChanges();
+    update(delta);
     render();
+    
+    timePrev = time;
     requestAnimationFrame(animate);
 }
 
 function render() {
     'use strict';
     
-    renderer.render(scene, currCamera);
+    renderer.autoClear = false; // render overlay on top of main scene
+
+    renderer.clear();
+    renderer.render(scene, camera);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.BasicShadowMap;
+
+    if (isPaused) {
+    	renderer.clearDepth();
+    	renderer.render(pauseHUD, pauseCamera);
+    }
 }
 
 function init() {
@@ -344,9 +514,9 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createPerspectiveCamera();
-
-    currCamera = camera;
+    createCameras();
+    createPauseHUD();
+    
     render();
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
